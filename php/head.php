c1a725fb3cbaa06994d5ed457a1a7c0e9605809b
Christoph Sauer <christoph.sauer@mni.thm.de>
2014-11-07 12:05:21 +0100

    add character encoding and head.php

---
+++ php/head.php
@@ -0,0 +1,6 @@
+<?php
+   echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
+   echo '<link href="css/bootstrap.min.css" rel="stylesheet">';
+   echo '<script src="js/bootstrap.min.js"></script>';
+
+?>
