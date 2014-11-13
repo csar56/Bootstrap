a5079a6505e1f0b1c662e249cf191b92a198e3c9
Christoph Sauer <christoph.sauer@mni.thm.de>
2014-11-13 11:10:35 +0100

    add bsfell homepage

--- index.php
+++ index.php
@@ -3,34 +3,27 @@
 
 <head>
 	<title>Christoph Sauer</title>
-		<?php
-		include('php/head.php');
-	?>
 
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+   	<link href="css/bootstrap.min.css" rel="stylesheet">
+   	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php
-	include('php/navbar.php');
-?>
+<head style="text-align:center;"><h1>IBS-Seite von Christoph Sauer</h1></head>
 
 
-	<main class="container">
-		<div class="row-fluid">
-			<div class="col-md-12">
-			<a href="http://www.codecademy.com/csar56"><h1>Codecademy</h1></a>
-			</div>
-			
-			<div class="col-md-6">
-				Gource Video von dieser Seite<br />
-				<video controls preload="auto" width="500" src="video/gource.webm"></video>
-			</div>
-			<div class="col-md-6">
-				Gource Video von <a href="http://adf.ly/toBEr">BS Fellingshausen</a><br />
-				<video controls preload="auto" width="500" src="video/BSFell.webm"></video>
-			</div>
-		</div>
-	</main>
-
+<div class="row-fluid">
+	<div class="col-md-1"></div>
+	<div class="col-md-5">
+		<h2>BS Seite (haupte Seite)</h2>
+		<a href="bs/index.php"><img src="img/bsfell.png" width="500px"/></a>
+	</div>
+	<div class="col-md-5">
+		<h2>2. Seite f&uuml;r IBS Unterricht</h2>
+		<a href="uni/index.php"><img src="img/uni.png" width="500px"/></a>
+	</div>
+	<div class="col-md-1"></div>
+</div>
 
 
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
