180ac82179dfa1ccc0affb8ca4aef8c460eafac5
csar56 <christoph.sauer@mni.thm.de>
2014-11-14 01:09:01 +0100

    change user counter

--- bs/m/impressum.php
+++ bs/m/impressum.php
@@ -175,14 +175,22 @@
 	
 	<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
-	<!-- GoStats JavaScript Based Code -->
-<script type="text/javascript" src="http://gostats.de/js/counter.js"></script>
-<script type="text/javascript">_gos='monster.gostats.de';_goa=476293;
-_got=5;_goi=1;_gol='statistik';_GoStatsRun();</script>
-<noscript><a target="_blank" title="statistik" 
-href="http://gostats.de"><img alt="statistik" 
-src="http://monster.gostats.de/bin/count/a_476293/t_5/i_1/counter.png" 
-style="border-width:0" /></a></noscript>
-<!-- End GoStats JavaScript Based Code -->
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10133377; 
+var sc_invisible=1; 
+var sc_security="72ad9c82"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify
+analytics ecommerce" href="http://statcounter.com/shopify/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10133377/0/72ad9c82/1/"
+alt="shopify analytics ecommerce"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
 </body>
 </html>
