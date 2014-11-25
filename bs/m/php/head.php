0f66bf6334bcc98a0cc0a6768cc32c25c3196fa1
Christoph Sauer <christoph.sauer@mni.thm.de>
2014-11-25 14:35:11 +0100

    remove bs site

--- bs/m/php/head.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
-echo'<link href="../css/bootstrap.min.css" rel="stylesheet">';
-echo'<link href="css/style.css" rel="stylesheet" />';
-
-echo'<meta name="viewport" content="width=device-width, initial-scale=1">';
-echo'<link rel="shortcut icon" href="../img/fox.ico" type="image/x-icon"/>';
-
-?>
