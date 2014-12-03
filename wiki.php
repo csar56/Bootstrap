ddfefb887a57b353a40d3e0a05d092b812445d54
Christoph Sauer <christoph.sauer@mni.thm.de>
2014-12-03 23:58:24 +0100

    piwik

--- wiki.php
+++ wiki.php
@@ -42,14 +42,14 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//christophsauer.cloudcontrolled.com/Bootstrap/piwik/";
+    var u="//christophsauer.cloudcontrolled.com/piwik/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//christophsauer.cloudcontrolled.com/Bootstrap/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//christophsauer.cloudcontrolled.com/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
